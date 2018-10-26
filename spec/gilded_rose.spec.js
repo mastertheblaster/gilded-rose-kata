301f17d84d566f757a720773a5df74a27f39a25e
Mantas Miliukas <mantasm@wix.com>
2018-10-26 22:19:12 +0300

    step 3: add basic rules

--- spec/gilded_rose.spec.js
+++ spec/gilded_rose.spec.js
@@ -3,7 +3,7 @@ const {Item, Shop} = require('../src/gilded_rose');
 
 describe('Gilded rose', () => {
 
-  it('should cover legacy functionality', function() {
+  it('should cover legacy functionality', () => {
     data.forEach(({ before, after }) => {
       const shop = new Shop([new Item(before.name, before.sellIn, before.quality)]);
       const [item] = shop.updateQuality();
@@ -12,6 +12,25 @@ describe('Gilded rose', () => {
       expect(item.quality).toEqual(after.quality);
     });
   });
+
+  it('All items have a SellIn value which denotes the number of days we have to sell the item', () => {
+    const item = new Item('', 1, 0);
+    expect(item.sellIn).toEqual(1);
+  });
+
+  it('All items have a Quality value which denotes how valuable the item is', () => {
+    const item = new Item('', 0, 1);
+    expect(item.quality).toEqual(1);
+  });
+
+  it('At the end of each day our system lowers both values for every item', () => {
+    const item = new Item('', 1, 1);
+    const shop = new Shop([item]);
+    const [updatedItem] = shop.updateQuality();
+    expect(updatedItem).toEqual(jasmine.objectContaining({
+      name: '', sellIn: 0, quality: 0
+    }));
+  })
 
 });
 
