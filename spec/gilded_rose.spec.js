578d17d2142077677fada13dc64451ff47b1f308
Mantas Miliukas <mantasm@wix.com>
2018-10-26 22:07:40 +0300

    step 2: add a test using generated data

--- spec/gilded_rose.spec.js
+++ spec/gilded_rose.spec.js
@@ -1,10 +1,17 @@
+const data = require('./generate-test-data.json');
 const {Item, Shop} = require('../src/gilded_rose');
 
 describe('Gilded rose', () => {
-  it('should foo', function() {
-    const gildedRose = new Shop([ new Item('foo', 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual('fixme');
+
+  it('should cover legacy functionality', function() {
+    data.forEach(({ before, after }) => {
+      const shop = new Shop([new Item(before.name, before.sellIn, before.quality)]);
+      const [item] = shop.updateQuality();
+      expect(item.name).toEqual(after.name);
+      expect(item.sellIn).toEqual(after.sellIn);
+      expect(item.quality).toEqual(after.quality);
+    });
   });
+
 });
 
